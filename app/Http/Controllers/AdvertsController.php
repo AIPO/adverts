0d41bd710aa4bb36bdca3062f20ae0f13501496f
povilas <p.vaitonis@gmail.com>
2017-10-15 13:31:23 +0300

    AdvertsController fixes for creating and showing adverts

--- app/Http/Controllers/AdvertsController.php
+++ app/Http/Controllers/AdvertsController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Advert;
 use Illuminate\Http\Request;
 
 class AdvertsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index', 'show');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -13,7 +19,12 @@ class AdvertsController extends Controller
      */
     public function index()
     {
-        //
+        $adverts = Advert::orderBy('created_at', 'desc')
+            ->get(['id', 'user_id', 'name', 'description', 'type', 'category_id', 'price']);
+        return response()->json([
+                'adverts' => $adverts,
+            ]
+        );
     }
 
     /**
@@ -23,38 +34,61 @@ class AdvertsController extends Controller
      */
     public function create()
     {
-        //
+        //$form =
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'description' => 'required|max:3000',
+            'image' => 'image',
+            'category_id' => 'required',
+            'price' => 'required'
+        ]);
+        if (!$request->hasFile('image') && !$request->file('image')->isValid()) {
+            return abort(404, 'Image not uploaded.');
+        }
+        $filename = $this->getFileName($request->image);
+        $request->image->move(base_path('public/images'), $filename);
+        $advert = new Advert($request->all());
+        $advert->image = $filename;
+        $request->user()->adverts()->save($advert);
+        return response()->json([
+            'saved' => true,
+            'id' => $advert->id,
+            'message' => 'Your advert is published'
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
+        $advert = Advert::with(['user', 'name', 'description'])
+            ->findOrFail($id);
+        return response()->json([
+            'adverts' => $advert,
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($id, Request $request)
     {
         //
     }
@@ -62,8 +96,8 @@ class AdvertsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -74,11 +108,16 @@ class AdvertsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         //
+    }
+
+    protected function getFileName($file)
+    {
+        return str_random(32) . '.' . $file->extention();
     }
 }
