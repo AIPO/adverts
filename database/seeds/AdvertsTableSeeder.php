49be3cb761dec643410b97717f3e33cd0fdd8581
povilas <p.vaitonis@gmail.com>
2017-10-16 11:12:53 +0300

    fixed seeder class

--- database/seeds/AdvertsTableSeeder.php
+++ database/seeds/AdvertsTableSeeder.php
@@ -14,10 +14,9 @@ class AdvertsTableSeeder extends Seeder
      */
     public function run()
     {
-
-
-        }
-
+        factory(App\User::class, 50)->create()->each(function ($u) {
+            $u->posts()->save(factory(App\Advert::class)->make());
+        });
 
     }
 }
